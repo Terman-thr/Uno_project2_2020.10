f2a3a08e77a4f0137d100b1e14122ccd93198d98
HaoranTang520370910096 <tanghaoran@sjtu.edu.cn>
2020-10-31 12:09:24 +0800

    10.31.noon finish showing the first card

--- main.c
+++ main.c
@@ -49,14 +49,10 @@ void ShuffleCard(int CardNum,int *card){
 }
 
 /*turn the number(0-51) to corresponding card
- * the string store the card; the number(0-51) of the card*/
-void Card2Str(char *str,int num) {
+ * the number(0-51) of the card*/
+void Card2Str(int num) {
     char *rank[4]={"J","Q","K","A"};
     char *suit[4]={"  Spades","  Hearts","Diamonds","   Clubs"};
-    if ((num%13+1)>10)
-        sprintf(str,"%s %s",suit[num/13],rank[num%13-9]);
-    else
-        sprintf(str,"%s %d",suit[num/13],num%13+2);
     if ((num % 13 + 1) > 10)
         printf("%s %s   ", suit[num / 13], rank[num % 13 - 9]);
     else
@@ -65,25 +61,50 @@ void Card2Str(char *str,int num) {
 }
 
 //Add cards to players: pointer of first card of stack; pointer of specific player
-void Dealer(const int *card, Player *player){
-    int i=0,j=0;
+void Dealer(int *card, Player *player){
+    int i=0,j=0,k=0;
     while (*(card+i)==-1) /* -1 represent for no card, draw card start from 0*/
     {   i++;}
     while ((*player).card[j]!=-1)
     {   j++;}
-    (*(player+j)).card[j]=*(card+i);
+    (*player).card[j]=*(card+i);
+    *(card+i)=-1;
 }
 
+//choose the first player
 int FirstCardCmp(Player *player,int n){
-    int num;/*the number of player with max card*/
-    int tmp=(*player).card[0];
+    int num=0;/*the number of player with max card*/
+    int tmp=player->card[0];
     for (int i=1;i<n;i++){
-        if (tmp<(*(player+i)).card[0]){
-            tmp=(*(player+i)).card[0];
-            num=i+1;
+        if (tmp<(player+i)->card[0]){
+            tmp=(player+i)->card[0];
+            num=i;
         }
     }
     return num;//if several player have same card, choose the previous player
+}
+
+/*Play a card .i.e add a card to Card_record
+ * the pointer of player; the sequence number of the card (0-MAX_CARD-1)*/
+void PlayACard(Player *player,int num,int *Card_record,int *Card_valid_Rec,int *DiscardPile){
+    int k=0;
+    if (*Card_valid_Rec==-1){
+        while(*(DiscardPile+k)!=-1)
+        {   k++;}
+        *(DiscardPile+k)=player->card[0];
+        player->card[0]=-1;
+    }
+    else{
+        *Card_record=player->card[num];
+        *Card_valid_Rec=player->card[num];
+        for (int i=num;i<MAX_CARD-1;i++){
+            player->card[i]=player->card[i+1];
+            player->card[MAX_CARD-1]=-1;
+        }/*tidy the card*/
+        while(*(DiscardPile+k)!=-1)
+        {   k++;}
+        *(DiscardPile+k)=*Card_valid_Rec;
+    }
 }
 
 //Add card from stack or initial stack to players
@@ -137,14 +158,14 @@ int main(int argc,char*argv[]) {
     }
     printf("Showing the number you set.\n r=%d\n d=%d\n n=%d\n c=%d\n filename=%s\n",r,d,n,c,filename);
 
-    //-------------------------------------the card part-------------------------------------------------------
+    //-------------------------------------the card part----------------------------------------------------------------
 
     //create n players, clean their hands and initialize their scores
     Player player[n];
     Player *player1=&player[0];
     for (int i=0;i<n;i++){
         player[i].score=0;
-        for (int j=1;j<=MAX_CARD;j++)
+        for (int j=0;j<MAX_CARD;j++)
             player[i].card[j]=-1;// -1>>no card
     }
 
@@ -162,18 +183,69 @@ int main(int argc,char*argv[]) {
     //show the Shuffled cards in  **demo**  mode
     if (a_bool) {
         printf("Initial cards stack:");
-        char sShowCard[52 * d][11];
         for (int j = 0; j < 52 * d; j++) {
             if (j%MAX_WIDE_NUM==0)
-                printf("\n\n");
-            Card2Str(sShowCard[j], *(card + j));
+                printf("\n");
+            Card2Str(*(card + j));
         }
     }
+
+    //Initialize the discard pile
+    int *DiscardPile=(int *)calloc(d*52,sizeof(int));
+    for (int i=0;i<d*52;i++){
+        *(DiscardPile+i)=-1;
+    }/* the card is now the pointer of the first 0  all -1*/
 
     //Draw the first card and compare
     for (int i=0;i<n;i++)
         Dealer(card,player1+i);
-    int first=FirstCardCmp(player1,n);
-    printf("%d",first);
+    int first=FirstCardCmp(player1,n);/* first : the first player showing cards*/
+    printf("\nDetermining the playing order...\n\n");
+    /* Show first cards in all mode*/
+    for (int i=0;i<n;i++){
+        printf("Player %d : ",i+1);
+        Card2Str(player[i].card[0]);
+        printf("\n");
+    }
+    printf("\nGame will start with player %d.\n\n\n",first+1);
+
+    //Create the play record and initialize it
+    int Card_Record=-1,Card_valid_Rec=-1;
+
+    //Discard the first card
+    for (int i=0;i<n;i++){
+        PlayACard((player1+i),0,&Card_Record,&Card_valid_Rec,DiscardPile);}
+
+    //give each player c cards
+    printf("Dealing cards...\n");
+    for (int i=0;i<n;i++){
+        for (int j=0;j<c;j++){
+            Dealer(card,(player1+i));}
+    }
+    if (a_bool){
+        for (int i=0;i<n;i++){
+            printf("\n Player %d:",i+1);
+            for (int j=0;j<c;j++){
+                Card2Str(player[i].card[j]);}
+        }
+    }
+
+    //-------------------------------------the play part (demo mode)----------------------------------------------------
+
+    //deal the first card of stack ;initialize the suit and rank
+    int k=0;
+    while (card[k]==-1){
+        k++;}
+    Card_Record=card[k];
+    Card_valid_Rec=card[k];
+    printf("\n\n==========================Game Start============================\n\nFirst card : ");
+    Card2Str(Card_valid_Rec);
+    //the play cards part
+    /* while (r>0){
+
+    }*/
+
+    free(card);
+    free(DiscardPile);
     return 0;
 }
